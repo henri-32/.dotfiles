99894dca05629254c3fe363a7c5b979bc37cdd30
henri-32 <henrikirchhoff@gmail.com>
2026-04-27 00:32:11 +0200

    Neue Keymaps und eigen keymap.lua file erstellt, und aus init.lua ausgetrennt

--- init.lua
+++ init.lua
@@ -1,8 +1,13 @@
--- ========= BASIC =========
+-- ========= CORE: LEADER & KEYMAPS =========
+vim.g.mapleader = " "
+require("config.keymaps").keymaps()
+
+-- ========= OPTIONS =========
 -- Performance-Modus (roh tippen)
 vim.o.paste = true
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.timeoutlen = 300
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.clipboard = "unnamedplus"
@@ -11,17 +16,18 @@ vim.o.smartindent = false
 vim.o.cindent = false
 vim.o.autoindent = false
 vim.o.formatoptions = ""
-vim.g.mapleader = " "
+vim.o.foldmethod = "marker"
+vim.o.foldmarker = "{{{,}}}"
+vim.o.foldcolumn = "1"
 vim.cmd("filetype indent off")
 
--- ========= THEME =========
+-- ========= UI: THEME =========
 require("catppuccin").setup({
-   flavour = "macchiato",
+    flavour = "macchiato",
 })
-
 vim.cmd.colorscheme("catppuccin")
 
--- ========= COLORCOLUMN =========
+-- ========= AUTOCMDS: FILETYPE OPTIONS =========
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "python",
     callback = function()
@@ -30,12 +36,12 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown", 
+    pattern = "markdown",
     callback = function()
         vim.opt_local.colorcolumn = "75"
         vim.opt_local.wrap = true
-        vim.opt_local.linebreak = true 
-    end, 
+        vim.opt_local.linebreak = true
+    end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
@@ -48,10 +54,9 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
--- ========= CMP =========
+-- ========= CMP: SETUP =========
 local cmp = require("cmp")
-local cmp_types = require("cmp.types")
-local autocomplete_enabled = true
+autocomplete_enabled = true
 
 cmp.setup({
     completion = {
@@ -71,7 +76,6 @@ cmp.setup({
                 fallback()
             end
         end, { "i", "s" }),
-
         ["<M-j>"] = cmp.mapping.select_next_item(),
         ["<M-k>"] = cmp.mapping.select_prev_item(),
         ["<Esc>"] = cmp.mapping.abort(),
@@ -89,7 +93,6 @@ local function should_trigger_completion()
 
     local line = vim.fn.getline(".")
     local char_before = line:sub(col, col)
-
     return not char_before:match("%s")
 end
 
@@ -97,11 +100,9 @@ local function trigger_completion()
     if vim.api.nvim_get_mode().mode ~= "i" then
         return
     end
-
     if not autocomplete_enabled then
         return
     end
-
     if not should_trigger_completion() then
         return
     end
@@ -110,27 +111,22 @@ local function trigger_completion()
 end
 
 local function schedule_completion()
-if not autocomplete_enabled then
-return
-end
+    if not autocomplete_enabled then
+        return
+    end
 
-timer:stop()
-timer:start(delay, 0, vim.schedule_wrap(trigger_completion))
+    timer:stop()
+    timer:start(delay, 0, vim.schedule_wrap(trigger_completion))
 end
 
 vim.api.nvim_create_autocmd("TextChangedI", {
-callback = function()
-cmp.close()
-schedule_completion()
-end
+    callback = function()
+        cmp.close()
+        schedule_completion()
+    end,
 })
 
-vim.keymap.set("n", "<leader>cc", function()
-    autocomplete_enabled = not autocomplete_enabled
-end)
-
--- ========= LSP BASE CONFIG =========
-
+-- ========= LSP: BASE CONFIG =========
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.general = {
     positionEncodings = { "utf-8" },
@@ -143,15 +139,15 @@ local base_config = {
     },
 }
 
--- ========= LSP =========
+-- ========= LSP: SERVERS =========
 vim.lsp.config("clangd", vim.tbl_deep_extend("force", base_config, {
     cmd = {
         "clangd",
-		"--query-driver=/usr/bin/avr-g++",
-		"--query-driver=/usr/bin/avr-gcc",
-		"--query-driver=/usr/bin/g++",
--- Ob nur im Build gesucht wird oder überall
--- Kostet vllt etwas viel performance auf T520
+        "--query-driver=/usr/bin/avr-g++",
+        "--query-driver=/usr/bin/avr-gcc",
+        "--query-driver=/usr/bin/g++",
+        -- Ob nur im Build gesucht wird oder überall
+        -- Kostet vllt etwas viel performance auf T520
         "--background-index=false",
         "--clang-tidy=false",
         "--all-scopes-completion=false",
@@ -160,20 +156,15 @@ vim.lsp.config("clangd", vim.tbl_deep_extend("force", base_config, {
         "--header-insertion=never",
     },
 }))
-
 vim.lsp.enable("clangd")
 
--- ---------- PYTHON LSP ----------
 vim.lsp.config("pyright", vim.tbl_deep_extend("force", base_config, {
-	on_attach = function(client)
-		client.server_capabilities.documentFormattingProvider = false
-	end,
+    on_attach = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+    end,
 }))
-
 vim.lsp.enable("pyright")
 
-
--- ---------- RUFF LSP ----------
 vim.lsp.config("ruff", vim.tbl_deep_extend("force", base_config, {
     cmd = { "ruff", "server" },
     flags = {
@@ -182,7 +173,7 @@ vim.lsp.config("ruff", vim.tbl_deep_extend("force", base_config, {
 }))
 vim.lsp.enable("ruff")
 
--- ========= DIAGNOSTICS =========
+-- ========= LSP: DIAGNOSTICS =========
 vim.diagnostic.config({
     update_in_insert = false,
     virtual_text = false,
@@ -190,89 +181,7 @@ vim.diagnostic.config({
     severity_sort = true,
 })
 
--- ========= LSP KEYMAPS =========
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
-vim.keymap.set("n", "gr", vim.lsp.buf.references)
-
-vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<leader>cf", function()
-    vim.lsp.buf.format({
-        async = false,
-        filter = function(client)
-            if vim.bo.filetype == "python" then
-                return client.name == "ruff"
-            elseif vim.bo.filetype == "cpp" then
-                return client.name == "clangd"
-            end
-            return true
-        end,
-    })
-end)
-vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float)
-vim.keymap.set("n", "<leader>do", vim.diagnostic.setloclist)
-
-vim.keymap.set("n", "<leader>dd", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "<leader>DD", vim.diagnostic.goto_next)
-
--- ========= TELESCOPE =========
-local telescope = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>ff", telescope.find_files)
-vim.keymap.set("n", "<leader>fg", telescope.live_grep)
-vim.keymap.set("n", "<leader>fb", telescope.buffers)
-
--- WINDOW SPLIT Mit neuer File ===============
-vim.keymap.set("n", "<leader>fs", function()
-vim.cmd("split")
-require("telescope.builtin").find_files()
-end)
-vim.keymap.set("n", "<leader>fv", function()
-vim.cmd("vsplit")
-require("telescope.builtin").find_files()
-end)
-
---WINDOW SPLIT Mit Grep Treffer ================
-vim.keymap.set("n", "<leader>gs", function()
-	vim.cmd("split")
-	require("telescope.builtin").live_grep()
-end)
-vim.keymap.set("n", "<leader>gv", function()
-	vim.cmd("vsplit")
-	require("telescope.builtin").live_grep()
-end)
-
-vim.keymap.set("n", "<leader>fd", function()
-require("telescope.builtin").lsp_document_symbols()
-end)
-vim.keymap.set("n", "<leader>fl", function()
-require("telescope.builtin").lsp_workspace_symbols()
-end)
-vim.keymap.set('n', '<leader>fw', function()
-  local word = vim.fn.expand("<cword>")
-  require('telescope.builtin').live_grep({
-    default_text = word
-  })
-end)
-
--- ========= WINDOWS =========
-vim.keymap.set("n", "<leader>hh", "<C-w>h")
-vim.keymap.set("n", "<leader>jj", "<C-w>j")
-vim.keymap.set("n", "<leader>kk", "<C-w>k")
-vim.keymap.set("n", "<leader>ll", "<C-w>l")
-vim.keymap.set("n", "<leader>wv", "<C-w>v")
-vim.keymap.set("n", "<leader>ws", "<C-w>s")
-vim.keymap.set("n", "<leader>q", "<C-w>c")
-vim.keymap.set("n", "<leader>ww", "<C-w>o")
-
--- ========= HIGHLIGHT CLEAR =========
-vim.keymap.set("n", "<Esc>", function()
-    vim.cmd("nohlsearch")
-end)
-
--- ========= AUTOCMD =========
+-- ========= AUTOCMDS: PASTE-MODE =========
 -- paste überall außer in Prompt/Spezial-Buffern
 vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "FileType" }, {
     callback = function()
@@ -291,45 +200,3 @@ vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "FileType" }, {
         end
     end,
 })
-
---================= TERMINAL ======================
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
-vim.keymap.set("n", "<leader>t", function()
-	vim.cmd("term")
-end)
-vim.keymap.set("n", "<leader>tt", function()
-	vim.cmd("split")
-	vim.cmd("term")
-end)
-
---================= QUICKFIX=======================
-vim.keymap.set("n", "<leader>n", "<cmd>cnext<CR>")
-vim.keymap.set("n", "<leader>N", "<cmd>cprev<CR>")
-vim.keymap.set("n", "<leader>qo", "<cmd>copen<CR>")
-vim.keymap.set("n", "<leader>qc", "<cmd>cclose<CR>") 
-
---================= NVIM OPERATIONS =======================
-vim.keymap.set("n", "<leader>o", "<C-o>", { desc = "Jump back in jumplist" })
-vim.keymap.set("n", "<leader>i", "<C-i>", { desc = "Jump forward in jumplist" })
-vim.keymap.set("n", "j", "gj", { noremap = true, silent = true })
-vim.keymap.set("n", "k", "gk", { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>fcy", function()
-	vim.fn.setreg("+", vim.fn.expand("%:p"))
-	vim.notify("absolute path copied to clipboard")
-end)
-vim.keymap.set("n", "<leader>fy", function()
-	vim.fn.setreg("+", vim.fn.expand("%"))
-	vim.notify("relative path copied to clipboard")
-end)
-vim.keymap.set("n", "<leader>w", function()
-	vim.cmd("write")
-end)
-vim.keymap.set("n", "<leader>wq", function()
-	vim.cmd("w")
-	vim.cmd("q")
-end)
-vim.keymap.set("n", "<leader>mb", ":term<CR>imake clean<CR>bear -- make ")
-vim.keymap.set("n", "<leader>m", ":make<CR>")
-vim.keymap.set("n", "<leader>lr", ":LspRestart<CR>")
-
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,137 @@
+local M = {}
+
+function M.keymaps()
+    local telescope = require("telescope.builtin")
+
+    -- ========= COMPLETION =========
+    vim.keymap.set("n", "<leader>cc", function()
+        autocomplete_enabled = not autocomplete_enabled
+    end)
+
+    -- ========= LSP: NAVIGATION =========
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references)
+
+    -- ========= LSP: ACTIONS =========
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+    vim.keymap.set("n", "<leader>lr", ":LspRestart<CR>")
+    vim.keymap.set("n", "<leader>cf", function()
+        vim.lsp.buf.format({
+            async = false,
+            filter = function(client)
+                if vim.bo.filetype == "python" then
+                    return client.name == "ruff"
+                elseif vim.bo.filetype == "cpp" then
+                    return client.name == "clangd"
+                end
+                return true
+            end,
+        })
+    end)
+
+    -- ========= DIAGNOSTICS =========
+    vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float)
+    vim.keymap.set("n", "<leader>do", vim.diagnostic.setloclist)
+    vim.keymap.set("n", "<leader>dd", vim.diagnostic.goto_prev)
+    vim.keymap.set("n", "<leader>DD", vim.diagnostic.goto_next)
+
+    -- ========= TELESCOPE: FILES / SEARCH =========
+    vim.keymap.set("n", "<leader>ff", telescope.find_files)
+    vim.keymap.set("n", "<leader>fg", telescope.live_grep)
+    vim.keymap.set("n", "<leader>fb", telescope.buffers)
+    vim.keymap.set("n", "<leader>fd", telescope.lsp_document_symbols)
+    vim.keymap.set("n", "<leader>fl", telescope.lsp_workspace_symbols)
+    vim.keymap.set("n", "<leader>fw", function()
+        local word = vim.fn.expand("<cword>")
+        telescope.live_grep({
+            default_text = word,
+        })
+    end)
+
+    -- ========= TELESCOPE: SPLIT WORKFLOWS =========
+    vim.keymap.set("n", "<leader>fs", function()
+        vim.cmd("split")
+        telescope.find_files()
+    end)
+    vim.keymap.set("n", "<leader>fv", function()
+        vim.cmd("vsplit")
+        telescope.find_files()
+    end)
+    vim.keymap.set("n", "<leader>gs", function()
+        vim.cmd("split")
+        telescope.live_grep()
+    end)
+    vim.keymap.set("n", "<leader>gv", function()
+        vim.cmd("vsplit")
+        telescope.live_grep()
+    end)
+
+    -- ========= WINDOWS =========
+    vim.keymap.set("n", "<leader>hh", "<C-w>h")
+    vim.keymap.set("n", "<leader>jj", "<C-w>j")
+    vim.keymap.set("n", "<leader>kk", "<C-w>k")
+    vim.keymap.set("n", "<leader>ll", "<C-w>l")
+    vim.keymap.set("n", "<leader>wv", "<C-w>v")
+    vim.keymap.set("n", "<leader>ws", "<C-w>s")
+    vim.keymap.set("n", "<leader>q", "<C-w>c")
+    vim.keymap.set("n", "<leader>ww", "<C-w>o")
+
+    -- ========= TERMINAL =========
+    vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+    vim.keymap.set("n", "<leader>t", function()
+        vim.cmd("term")
+    end)
+    vim.keymap.set("n", "<leader>tt", function()
+        vim.cmd("split")
+        vim.cmd("term")
+    end)
+
+    -- ========= QUICKFIX =========
+    vim.keymap.set("n", "<leader>n", "<cmd>cnext<CR>")
+    vim.keymap.set("n", "<leader>N", "<cmd>cprev<CR>")
+    vim.keymap.set("n", "<leader>qo", "<cmd>copen<CR>")
+    vim.keymap.set("n", "<leader>qc", "<cmd>cclose<CR>")
+    vim.keymap.set("n", "<leader>qf", function()
+        vim.cmd("cfile neovim_utils/quickfix_list.txt")
+        vim.cmd("copen")
+    end)
+
+
+    -- ========= EDITOR NAVIGATION =========
+    vim.keymap.set("n", "<leader>o", "<C-o>", { desc = "Jump back in jumplist" })
+    vim.keymap.set("n", "<leader>i", "<C-i>", { desc = "Jump forward in jumplist" })
+    vim.keymap.set("n", "j", "gj", { noremap = true, silent = true })
+    vim.keymap.set("n", "k", "gk", { noremap = true, silent = true })
+    vim.keymap.set("n", "<Esc>", function()
+        vim.cmd("nohlsearch")
+    end)
+	vim.keymap.set("n", "<leader>e", "<cmd>e .<CR>")
+	vim.keymap.set("n", "<leader>ee", function ()
+		vim.cmd("split") 
+		vim.cmd("e .")
+	end)
+
+    -- ========= FILE PATH COPYING =========
+    vim.keymap.set("n", "<leader>fcy", function()
+        vim.fn.setreg("+", vim.fn.expand("%:p"))
+        vim.notify("absolute path copied to clipboard")
+    end)
+    vim.keymap.set("n", "<leader>fy", function()
+        vim.fn.setreg("+", vim.fn.expand("%"))
+        vim.notify("relative path copied to clipboard")
+    end)
+
+    -- ========= SAVE / QUIT =========
+    vim.keymap.set("n", "<leader>w", function()
+        vim.cmd("w")
+    end)
+    vim.keymap.set("n", "<leader>wq", function()
+        vim.cmd("w")
+        vim.cmd("q")
+    end)
+end
+
+return M
