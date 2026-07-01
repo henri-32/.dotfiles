ae7f15f9ffaccc5a2dbf57f70132c092bad36701
henri-32 <henrikirchhoff@gmail.com>
2026-07-01 14:28:59 +0000

    .

---
+++ bash/bash_aliases.sh
@@ -0,0 +1,10 @@
+alias tmux_coding="$HOME/projects/scripts/setup_scripts/tmux_coding.sh"
+alias nvim-server="nvim --listen /tmp/nvim-main.sock"
+alias lucid='chromium --ozone-platform=wayland --app=https://lucid.app'
+alias chat='chromium --ozone-platform=wayland --app=https://chatgpt.com'
+alias projectstat="$HOME/scripts/status.sh"
+alias power="acpi"
+alias chart='flatpak run org.opencpn.OpenCPN'
+alias weather='XyGrib'
+alias notices='/usr/bin/Logseq-linux-x64-0.10.15.AppImage'
+
